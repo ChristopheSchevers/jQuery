fc89f60422dd41cd93864b3b54d0b5056eb6dc9c
Christophe Schevers <christophe.schevers@gmail.com>
2019-03-27 20:17:07 +0100

    New commit

--- jQuery/jquery-exercises-3/exercises_5/main.js
+++ jQuery/jquery-exercises-3/exercises_5/main.js
@@ -1,34 +1,49 @@
+/* const vwHeight = $(window).height();
+const vwWidth = $(window).width();
+console.log(vwHeight);
+console.log(vwWidth);
+
 let pos = $("#square").position();
 let posX = pos.left;
 let posY = pos.top;
+
+if (posX < 0) {
+    posX = vwWidth;
+} else if (posX > vwWidth) {
+    posX = 0;
+} else if (posY < 0) {
+    posY = vwHeight;
+} else if (posY > vwHeight) {
+    posY = 0;
+} */
 
 $(document).keydown(function(e){
     switch (e.which) {
         // Left
         case 37:
-        $("#square").finish().animate({
-            left: "-= 10"
+        $("#square").css({
+            left: "-=10"
         });
         break;
 
         // Up
         case 38:
-        $("#square").finish().animate({
-            top: "-= 10"
+        $("#square").css({
+            top: "-=10"
         });
         break;
 
         // Right
         case 39:
-        $("#square").finish().animate({
-            left: "+= 10"
+        $("#square").css({
+            left: "+=10"
         });
         break;
 
         // Down
         case 40:
-        $("#square").finish().animate({
-            top: "+= 10"
+        $("#square").css({
+            top: "+=10"
         });
         break;
 
